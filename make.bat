2ca378fdfc17c37607ca401f936008da52bc5bfc
Kieran Connell <kieran@kconnell.co.uk>
2020-06-04 20:08:36 +0100

    Add !folder build to make.bat.

--- make.bat
+++ make.bat
@@ -1,3 +1,28 @@
 @echo off
+
+if NOT EXIST build mkdir build
+echo Assembling code...
 ..\..\vasm\vasmarm_std_win32.exe -L compile.txt -m250 -Fbin -opt-adr -o build\stniccc.bin stniccc.asm
-if %ERRORLEVEL%==0 copy build\stniccc.bin "..\..\Arculator_V2.0_Windows\hostfs\stniccc,ff8"
+
+if %ERRORLEVEL% neq 0 (
+	echo Failed to assemble code.
+	exit /b 1
+)
+
+echo Building assets...
+python bin\png2arc.py -pad -o build\title.bin -p build\title.pal data\title-16.png 9
+
+echo Making !folder...
+set FOLDER="!BSAATT"
+if EXIST %FOLDER% del /Q "%FOLDER%"
+if NOT EXIST %FOLDER% mkdir %FOLDER%
+
+echo Adding files...
+copy folder\*.* "%FOLDER%\*.*"
+copy build\stniccc.bin "%FOLDER%\!RunImage,ff8"
+
+echo Copying !folder...
+set HOSTFS=..\..\Arculator_V2.0_Windows\hostfs
+if EXIST "%HOSTFS%\%FOLDER%" del /Q "%HOSTFS%\%FOLDER%"
+if NOT EXIST "%HOSTFS%\%FOLDER%" mkdir "%HOSTFS%\%FOLDER%"
+copy "%FOLDER%\*.*" "%HOSTFS%\%FOLDER%"
