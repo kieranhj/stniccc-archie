23b764de6894eb6849a335fdc7423c87faaac30d
Kieran Connell <kieran@kconnell.co.uk>
2020-06-20 18:01:44 +0100

    Updated credits and music.

--- make.bat
+++ make.bat
@@ -74,7 +74,7 @@ echo Adding files...
 copy folder\*.* "%FOLDER%\*.*"
 copy build\stniccc.bin "%FOLDER%\!RunImage,ff8"
 copy data\stniccc\scene1.bin "%FOLDER%\Scene1,ffd"
-copy data\music\Arc-NIC9.mod "%FOLDER%\Music,001"
+copy data\music\Arc-NIC10.mod "%FOLDER%\Music,001"
 copy data\readme.txt "%FOLDER%\Readme,fff"
 
 echo Copying !folder...
